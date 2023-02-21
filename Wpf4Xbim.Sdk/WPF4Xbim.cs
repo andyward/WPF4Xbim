e18824d0933113c41f723ebeaee6a02d581dd4b6
Andy Ward <andy.ward@xbim.net>
2023-02-21 11:48:55 +0000

    Add project files.

---
+++ Wpf4Xbim.Sdk/WPF4Xbim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Wpf4Xbim.Sdk
+{
+    [Transaction(TransactionMode.Manual)]
+    public class WPF4Xbim : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {       
+            MainWindow wpf = new MainWindow();
+            wpf.Show();
+            return Result.Succeeded;
+        }
+    }
+}
